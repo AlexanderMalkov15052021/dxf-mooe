075f037f6ad3817804d44796ff1e7a13140d8fd0
Alex Malkov <malkov.fighter@yandex.ru>
2024-11-07 22:41:28 +0700

    Fix layer rect

--- src/modules/insert/setLayerSize.ts
+++ src/modules/insert/setLayerSize.ts
@@ -4,13 +4,13 @@ import { Coords, MooeDoc } from "@/types";
 
 export const setLayerSize = (mooeDoc: MooeDoc, layer: any, origin: Coords) => {
 
-    const width = getDistTwoPoints(
+    const height = getDistTwoPoints(
         (layer?.vertices[0].x + origin.x),
         (layer?.vertices[0].y + origin.y),
         (layer?.vertices[1].x + origin.x),
         (layer?.vertices[1].y + origin.y)
     );
-    const height = getDistTwoPoints(
+    const width = getDistTwoPoints(
         (layer?.vertices[0].x + origin.x),
         (layer?.vertices[0].y + origin.y),
         (layer?.vertices[3].x + origin.x),
@@ -23,10 +23,10 @@ export const setLayerSize = (mooeDoc: MooeDoc, layer: any, origin: Coords) => {
     mooeDoc.mSceneMap.mGridMsg.info.width = Math.floor(width * scaleCorrection / 0.05);
     mooeDoc.mSceneMap.mGridMsg.info.height = Math.floor(height * scaleCorrection / 0.05);
 
-    mooeDoc.mSceneMap.mMapAttr.mMapOrigin.x = (layer?.vertices[1].x + origin.x) * scaleCorrection;
-    mooeDoc.mSceneMap.mMapAttr.mMapOrigin.y = (layer?.vertices[1].y + origin.y) * scaleCorrection;
+    mooeDoc.mSceneMap.mMapAttr.mMapOrigin.x = (layer?.vertices[0].x + origin.x) * scaleCorrection;
+    mooeDoc.mSceneMap.mMapAttr.mMapOrigin.y = (layer?.vertices[0].y + origin.y) * scaleCorrection;
 
-    mooeDoc.mSceneMap.mGridMsg.info.origin.position.x = (layer?.vertices[1].x + origin.x) * scaleCorrection;
-    mooeDoc.mSceneMap.mGridMsg.info.origin.position.y = (layer?.vertices[1].y + origin.y) * scaleCorrection;
+    mooeDoc.mSceneMap.mGridMsg.info.origin.position.x = (layer?.vertices[0].x + origin.x) * scaleCorrection;
+    mooeDoc.mSceneMap.mGridMsg.info.origin.position.y = (layer?.vertices[0].y + origin.y) * scaleCorrection;
 
 }
